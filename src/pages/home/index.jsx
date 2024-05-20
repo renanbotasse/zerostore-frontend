5adc6fe68b04e9572f2c30ab2c21e88d245e467a
rbotasse <renan.botasse@gohike.app>
2024-05-20 16:31:15 +0100

    feat: add header

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,13 @@ import Footer from '../../components/footer/index'
 import DisplayHorizontalBlock from "../../components/banner";
 import Hero from "../../components/herobanner/index"
 import TopNav from '../../components/topnavbar';
+import NavBar from '../../components/navbar';
 import '../../globals.css';
 
 export default function Home() {
     return (
         <div>
-            <TopNav/>
+            <NavBar/>
             <Hero/>
             <DisplayHorizontalBlock/>
             <Footer />
