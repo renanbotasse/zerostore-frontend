5c8c7cf5f92744bbcebf9dd06ed8869ab5367b1f
rbotasse <renan.botasse@gohike.app>
2024-07-17 14:00:35 +0100

    feat: add build chunkSize 1000

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000 // Increasing the warning limit to 1000 kB
+  }
 })
